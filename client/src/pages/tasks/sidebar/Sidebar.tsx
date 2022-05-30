bbadbba4321cf5a3f1bc856f76f84c6ceed06343
M-Xue <maxxue1512@gmail.com>
2022-05-31 00:35:30 +1000

    got the sidebar working

--- client/src/pages/tasks/sidebar/Sidebar.tsx
+++ client/src/pages/tasks/sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './sidebar.css'
 
 import styled from 'styled-components';
@@ -10,68 +10,53 @@ const SidebarContainer = styled.div.attrs((props: any) => ({
     width: props.sidebarWidth,
   }
 }))` 
+  
+`
+const SidebarBorderSpan = styled.span`
+  background-color: ${(props: any) => props.isResizeActive ? "rgb(38, 132, 255)" : ""};
 `
 
 
 
 export default function Sidebar() {
 
-  const [originalSidebarWidth, setOriginalSidebarWidth] = useState(267) // Need to make this based on local storage
-  const [sidebarWidth, setsidebarWidth] = useState(originalSidebarWidth);
 
-  const [originalMousePositionX, setOriginalMousePositionX] = useState(sidebarWidth)
-  
+  const originalSidebarWidth = useRef<number>(267) // Need to make this based on local storage
 
-  const [isResizeActive, setIsResizeActive] = useState(false)
+  const [sidebarWidth, setsidebarWidth] = useState<number>(originalSidebarWidth.current);
+
+  const originalMousePositionX = useRef<number>(sidebarWidth)
+  const isResizeActive = useRef<boolean>(false) 
+  const [isResizeActiveState, setIsResizeActiveState] = useState<boolean>(false)
 
 
   const resizeMouseDownHandler = (e: any) => {
-    setOriginalMousePositionX(e.clientX);
+    originalMousePositionX.current = e.clientX;
+
     document.addEventListener('mousemove', resizeMouseMoveHandler);
     document.addEventListener('mouseup', resizeMouseUpHandler);
   
-    document.body.style.cursor = "ew-resize"; // Theres a bug here where the style of the cursor changes to default when you move outside the sidebar
-
-    console.log('mousedown');
-
-
-    setIsResizeActive(true);
-
+    document.body.style.cursor = "ew-resize"; // ! BUG: Theres a bug here where the style of the cursor changes to default when you move outside the sidebar
+    isResizeActive.current = true; 
+    setIsResizeActiveState(prev => !prev) // This is to change the background color of the border when the user lets go of resizing the sidebar
   }
   
-
-
   const resizeMouseMoveHandler = (e: any) => {
-    if (originalMousePositionX) {
-      const mouseMovementX = e.clientX - originalMousePositionX;
-      const newWidth = originalSidebarWidth + mouseMovementX;
-      setsidebarWidth(newWidth)
-      console.log(sidebarWidth);
-
-    }
-    
-
-    console.log('mousemove');
+    const mouseMovementX = e.clientX - originalMousePositionX.current;
+    const newWidth = sidebarWidth + mouseMovementX;
+    setsidebarWidth(newWidth)
   }
 
-
   const resizeMouseUpHandler = (e:any) => {
-    setOriginalSidebarWidth(sidebarWidth);
+    originalSidebarWidth.current = sidebarWidth;
     document.body.style.cursor = "default"; 
 
     document.removeEventListener('mousemove', resizeMouseMoveHandler);
     document.removeEventListener('mouseup', resizeMouseUpHandler);
-    console.log('mouseup');
 
-
-    console.log(isResizeActive);
-    // setIsResizeActive(false);
-
+    isResizeActive.current = false; 
+    setIsResizeActiveState(prev => !prev) // This is to change the background color of the border when the user lets go of resizing the sidebar
   }
-
-
-  
-
 
   return (
 
@@ -79,19 +64,16 @@ export default function Sidebar() {
     <SidebarContainer 
       className='sidebar-container'
       sidebarWidth={sidebarWidth}
+      isResizeActive={isResizeActive.current}
     >
       <div className="sidebar-content">sdfhgdfs</div>
-
 
       <div className="sidebar-resize-container">
         <div className="sidebar-border"></div>
         <button className='sidebar-resize-button' onMouseDown={resizeMouseDownHandler}>
-          <span></span>
+          <SidebarBorderSpan isResizeActive={isResizeActive.current}></SidebarBorderSpan>  
         </button>
       </div>
-
-      
-
     </SidebarContainer>
   )
 }
@@ -117,186 +99,144 @@ export default function Sidebar() {
 
 
 
-/* <Resizable
-        style={style}
-        defaultSize={{
-          width: 200,
-          height: 200
-        }}
-      >
-        001
-      </Resizable> */
 
 
 
 
 
 
-// * ////// OG
-      // export default function Sidebar() {
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export default function Sidebar() {
+
+
+  //   // const originalSidebarWidth = useRef(267) // Need to make this based on local storage
   
+  //   const [originalSidebarWidth, setOriginalSidebarWidth] = useState(267) // Need to make this based on local storage
+  //   const [sidebarWidth, setsidebarWidth] = useState(originalSidebarWidth);
   
-      //   return (
-      
-      
-      //     <div className='sidebar-container'>
-      
-      
-      
-      //       <div className="sidebar-content"></div>
-      
-      
-      //       <div className="sidebar-resize-container">
-      //         <div className="sidebar-border"></div>
-      //         <button className='sidebar-resize-button' >
-      //           <span></span>
-      //         </button>
-      //       </div>
-      
-            
-      
-      //     </div>
-      //   )
-      // }
-
-// * //
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      // {/* <button className='collapse-button'>
-      //   <svg width="24" height="24" viewBox="0 0 24 24" role="presentation"><path d="M10.294 9.698a.988.988 0 010-1.407 1.01 1.01 0 011.419 0l2.965 2.94a1.09 1.09 0 010 1.548l-2.955 2.93a1.01 1.01 0 01-1.42 0 .988.988 0 010-1.407l2.318-2.297-2.327-2.307z" fill="currentColor" fill-rule="evenodd"></path></svg>
-      // </button> */}
-
-
-
-
-
-
-// // const [sidebarFlyoutWidth, setSidebarFlyoutWidth] = useState(sidebarElementRef.current?.clientWidth)
-//   // const resizeButtonRef = useRef<HTMLButtonElement>(null);
-
-
-//   const sidebarElementRef = useRef<HTMLDivElement>(null);
-
-//   const [sidebarWidth, setSidebarWidth] = useState(267)
-
-//   const [currMousePositionX, setCurrMousePositionX] = useState(0)
-//   const [originalWidth, setOriginalWidth] = useState(0)
-
-
-
-
-
-
-
-
-
-
-//   useEffect(() => {
-//     console.log(sidebarWidth);
-
-//   }, [sidebarWidth])
-  
-
-
-  
-//   const resizeMouseDownHandler = (e: any) => {
-//     // console.log('mousedown');
+  //   const [originalMousePositionX, setOriginalMousePositionX] = useState(sidebarWidth)
     
-//     setCurrMousePositionX(e.clientX);
-
-
-//     sidebarElementRef.current && setOriginalWidth(sidebarElementRef.current?.clientWidth)
-
-
-
-//     sidebarElementRef.current && sidebarElementRef.current.addEventListener('mousemove', resizeMouseMoveHandler);
-//     sidebarElementRef.current && sidebarElementRef.current.addEventListener('mouseup', resizeMouseUpHandler);
-
-
-
-//   }
-
-
-//   const resizeMouseMoveHandler = (e: any) => {
-//     const dx = e.clientX - currMousePositionX;
-
-
-
-//     originalWidth && setSidebarWidth(dx + originalWidth);
-//     // console.log('mousemove');
-//   }
-
-
-//   const resizeMouseUpHandler = (e: any) => {
-//     // console.log('mouseup');
-
-//     sidebarElementRef.current && sidebarElementRef.current.removeEventListener('mousemove', resizeMouseMoveHandler);
-//     sidebarElementRef.current && sidebarElementRef.current.removeEventListener('mouseup', resizeMouseUpHandler);
-//   }
   
-
-
-
+  //   const [isResizeActive, setIsResizeActive] = useState(false)
+  //   const test = useRef(false)
   
-
-
-//   // This event handler is just for edge case scenario. If the users mouse leaves the resize button, the mousemove event should be removed so you don't keep adjusting the width of the sidebar.
-//   // const resizeMouseLeaveHandler = (e: any) => {
-//   //   console.log('mouseleave');
-//   //   sidebarElementRef.current && sidebarElementRef.current.removeEventListener('mousemove', resizeMouseMoveHandler);
-//   //   sidebarElementRef.current && sidebarElementRef.current.removeEventListener('mouseup', resizeMouseUpHandler);
-//   // }
-
-//   //onMouseLeave={resizeMouseLeaveHandler}
-
-
-
-
-// return (
-//   <div className='sidebar-container' ref={sidebarElementRef} style={{width: sidebarWidth}}>
-
-//     <div className="sidebar-content"></div>
-
-
-//     <div className="sidebar-resize-container">
-//       <div className="sidebar-border"></div>
-//       <button className='sidebar-resize-button'  onMouseDown={resizeMouseDownHandler}>
-//         <span></span>
-//       </button>
-//     </div>
-
-
-
-
-
-//     {/* <button className='collapse-button'>
-//       <svg width="24" height="24" viewBox="0 0 24 24" role="presentation"><path d="M10.294 9.698a.988.988 0 010-1.407 1.01 1.01 0 011.419 0l2.965 2.94a1.09 1.09 0 010 1.548l-2.955 2.93a1.01 1.01 0 01-1.42 0 .988.988 0 010-1.407l2.318-2.297-2.327-2.307z" fill="currentColor" fill-rule="evenodd"></path></svg>
-//     </button> */}
-
-//   </div>
-// )
+  //   const resizeMouseDownHandler = (e: any) => {
+  //     setOriginalMousePositionX(e.clientX);
+  //     document.addEventListener('mousemove', resizeMouseMoveHandler);
+  //     document.addEventListener('mouseup', resizeMouseUpHandler);
+    
+  //     document.body.style.cursor = "ew-resize"; // Theres a bug here where the style of the cursor changes to default when you move outside the sidebar
+  
+  //     console.log('mousedown');
+  
+  
+  //     setIsResizeActive(true);
+  
+  //     console.log('MD test value: ', test);
+  //     test.current = true;
+  
+  //   }
+    
+  
+  
+  //   const resizeMouseMoveHandler = (e: any) => {
+  //     if (originalMousePositionX) {
+  //       const mouseMovementX = e.clientX - originalMousePositionX;
+  //       const newWidth = originalSidebarWidth + mouseMovementX;
+  //       setsidebarWidth(newWidth)
+  //       console.log(sidebarWidth);
+  
+  //     }
+      
+  
+  //     console.log('mousemove');
+  //   }
+  
+  
+  //   const resizeMouseUpHandler = (e:any) => {
+  //     setOriginalSidebarWidth(sidebarWidth);
+  //     document.body.style.cursor = "default"; 
+  
+  //     document.removeEventListener('mousemove', resizeMouseMoveHandler);
+  //     document.removeEventListener('mouseup', resizeMouseUpHandler);
+  //     console.log('mouseup');
+  
+  
+  //     console.log(isResizeActive);
+  //     // setIsResizeActive(false);
+  
+  
+  //     console.log('MU test value: ', test);
+  //     test.current = false;
+  
+  
+  //   }
+  
+  //   return (
+  
+  
+  //     <SidebarContainer 
+  //       className='sidebar-container'
+  //       sidebarWidth={sidebarWidth}
+  //     >
+  //       <div className="sidebar-content">sdfhgdfs</div>
+  
+  
+  //       <div className="sidebar-resize-container">
+  //         <div className="sidebar-border"></div>
+  //         <button className='sidebar-resize-button' onMouseDown={resizeMouseDownHandler}>
+  //           <span></span>
+  //         </button>
+  //       </div>
+  
+        
+  
+  //     </SidebarContainer>
+  //   )
+  // }
+  
