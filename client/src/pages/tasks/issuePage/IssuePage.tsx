f2748d5673c85d96d54b0336105d6bcbd4cde2cf
M-Xue <maxxue1512@gmail.com>
2022-08-17 13:58:52 +1000

    pushing everything to add to mac

---
+++ client/src/pages/tasks/issuePage/IssuePage.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './issuePage.css'
+
+export default function IssuePage() {
+  return (
+    <div className='issue-page-container'>
+      <div className="issue-content">
+        <div className="issue-content-title">Title</div>
+        <div className="issue-content-description">description</div>
+        <div className="issue-content-history">history</div>
+        <div className="issue-content-comments">comments</div>
+
+        <br/>
+
+        NEW CONCEPT FOR THIS. yOU WANT TO BE ABLE TO SHOWCASE YOUR PROJECT AS YOU MAKE IT
+        LETS THIS PLATFORM LET U SHOW UR STUFF
+        MAYBE CLICK A BUTTON THAT SAYS "DISPLAY" AND YOUR PAGE FOR YOU PROJECT CAN BE SHOW CASED
+
+      </div>
+      <div className="issue-details"></div>
+    </div>
+  )
+}
