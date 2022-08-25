ed19104c1f726bdab9633a512b9d882ebbd88289
M-Xue <maxxue1512@gmail.com>
2022-08-25 19:49:47 +1000

    fixed kanban typescript

--- client/src/pages/tasks/Tasks.tsx
+++ client/src/pages/tasks/Tasks.tsx
@@ -13,15 +13,6 @@ import IssuePage from './issuePage/IssuePage'
 
 export default function Tasks() {
 
-  // const Button = styled.button`
-  //   background-colour: cyan;
-  //   font-size: 14px;
-  //   padding: 10px
-  // `
-
-
-
-
   return (
     <div className='task-page-container'>
       <Sidebar />
--- client/src/pages/tasks/board/kanbanBoard/KanbanBoard.tsx
+++ client/src/pages/tasks/board/kanbanBoard/KanbanBoard.tsx
@@ -3,149 +3,20 @@ import './kanbanBoard.css'
 import styled from 'styled-components';
 import KanbanBoardCard from '../kanbanBoardCard/KanbanBoardCard';
 
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { DragDropContext, Droppable, Draggable, DropResult, DroppableProvided, DraggableProvided } from "react-beautiful-dnd";
 import KanbanBoardHeading from '../kanbanBoardHeading/KanbanBoardHeading';
 
-interface columnData {
-	
-}
-
 const cardDummyData = {
-	"Backlog": [
-		{
-			id: '1',
-			listId: 'backlog',
-			priority: 'High',
-			project: 'Velocity',
-			title: 'Test1',
-			tags: ['UI', "Backend"],
-			deadline: '15 Jun 2022'
-		},
-		{
-			id: '10',
-			listId: 'backlog',
-			priority: 'High',
-			project: 'Velocity',
-			title: 'Test2',
-			tags: ['UI', "Backend"],
-			deadline: '15 Jun 2022'
-		},{
-			id: '3',
-			listId: 'backlog',
-			priority: 'High',
-			project: 'Velocity',
-			title: 'Test3',
-			tags: ['UI', "Backend"],
-			deadline: '15 Jun 2022'
-		},{
-			id: '4',
-			listId: 'backlog',
-			priority: 'High',
-			project: 'Velocity',
-			title: 'Test4',
-			tags: ['UI', "Backend"],
-			deadline: '15 Jun 2022'
-		},{
-			id: '5',
-			listId: 'backlog',
-			priority: 'High',
-			project: 'Velocity',
-			title: 'Test5',
-			tags: ['UI', "Backend"],
-			deadline: '15 Jun 2022'
-		},{
-			id: '6',
-			listId: 'backlog',
-			priority: 'High',
-			project: 'Velocity',
-			title: 'Test6',
-			tags: ['UI', "Backend"],
-			deadline: '15 Jun 2022'
-		},{
-			id: '7',
-			listId: 'backlog',
-			priority: 'High',
-			project: 'Velocity',
-			title: 'Test7',
-			tags: ['UI', "Backend"],
-			deadline: '15 Jun 2022'
-		},{
-			id: '8',
-			listId: 'backlog',
-			priority: 'High',
-			project: 'Velocity',
-			title: 'Test8',
-			tags: ['UI', "Backend"],
-			deadline: '15 Jun 2022'
-		},{
-			id: '9',
-			listId: 'backlog',
-			priority: 'High',
-			project: 'Velocity',
-			title: 'Test9',
-			tags: ['UI', "Backend"],
-			deadline: '15 Jun 2022'
-		},{
-			id: '11',
-			listId: 'backlog',
-			priority: 'High',
-			project: 'Velocity',
-			title: 'Test11',
-			tags: ['UI', "Backend"],
-			deadline: '15 Jun 2022'
-		},{
-			id: '12',
-			listId: 'backlog',
-			priority: 'High',
-			project: 'Velocity',
-			title: 'Test12',
-			tags: ['UI', "Backend"],
-			deadline: '15 Jun 2022'
-		}
-		,{
-			id: '13',
-			listId: 'backlog',
-			priority: 'High',
-			project: 'Velocity',
-			title: 'Test13',
-			tags: ['UI', "Backend"],
-			deadline: '15 Jun 2022'
-		},{
-			id: '14',
-			listId: 'backlog',
-			priority: 'High',
-			project: 'Velocity',
-			title: 'Test14',
-			tags: ['UI', "Backend"],
-			deadline: '15 Jun 2022'
-		},{
-			id: '15',
-			listId: 'backlog',
-			priority: 'High',
-			project: 'Velocity',
-			title: 'Test15',
-			tags: ['UI', "Backend"],
-			deadline: '15 Jun 2022'
-		},{
-			id: '16',
-			listId: 'backlog',
-			priority: 'High',
-			project: 'Velocity',
-			title: 'Test16',
-			tags: ['UI', "Backend"],
-			deadline: '15 Jun 2022'
-		},{
-			id: '17',
-			listId: 'backlog',
-			priority: 'High',
-			project: 'Velocity',
-			title: 'Test17',
-			tags: ['UI', "Backend"],
-			deadline: '15 Jun 2022'
-		}
-		
-	],
-	"To Do": [{
+	'backlog': [{
+		id: '1',
+		listId: 'backlog',
+		priority: 'High',
+		project: 'Velocity',
+		title: 'Test1',
+		tags: ['UI', "Backend"],
+		deadline: '15 Jun 2022'
+	}],
+	'toDo': [{
 		id: '2',
 		listId: 'backlog',
 		priority: 'High',
@@ -154,87 +25,88 @@ const cardDummyData = {
 		tags: ['UI', "Backend"],
 		deadline: '15 Jun 2022'
 	}],
-	"In Progress": [],
-	"In Review": [],
-	"Done": [],
-	"Cancelled": []
+	'inProgress': [],
+	'inReview': [],
+	'done': [],
+	'cancelled': []
+}
+interface kanbanBoard {
+	[key: string]: cardData[],
 }
 
 interface cardData {
-	id: String,
-	listId: String,
-	priority: String,
-	project: String,
-	title: String,
-	tags: String[],
-	deadline: String
+	id: string,
+	listId: string,
+	priority: string,
+	project: string,
+	title: string,
+	tags: string[],
+	deadline: string
 }
 
-
-
 // Removes and element from a given list from the given index and returns both the new list and the removed element
-const removeFromList = (list: cardData[], index: number): (cardData | cardData[])[] => {  
-	const result = Array.from(list);  
-	const [removed] = result.splice(index, 1);  
-	return [removed, result];
+const removeFromList = (list: cardData[], index: number) => {  
+	const resultList: cardData[] = Array.from(list);  
+	const removed: cardData = resultList.splice(index, 1)[0];  
+	return [removed, resultList];
 }
 
 // Returns a new list that is a copy of the given list with the given element inserted at the given index
-const addToList = (list: cardData[], index: number, element: cardData): cardData[] => {  
-	const result = Array.from(list);  
+const addToList = (list: cardData[], index: number, element: cardData) => {  
+	const result: cardData[] = Array.from(list);  
 	result.splice(index, 0, element);  
 	return result;
 }
 
-export default function KanbanBoard() {
-  const [board, setBoard] = useState<any>(cardDummyData);
 
-	const onDragEnd = (result: any) => {  
-		console.log(board)
-		console.log(result);
+
+export default function KanbanBoard() {
+  	const [board, setBoard] = useState<kanbanBoard>(cardDummyData);
+
+	// console.log("rendered");
+
+	const onDragEnd = (result: DropResult) => {
+		// console.log(result);
 		if (!result.destination) {  
 				return;  
 		}  
-		const listCopy = { ...board }  
-	
-		const sourceList = listCopy[result.source.droppableId] // result.source.droppableId is the key of the key value pair of the list in the elements state object that the draggable was taken from
-		const [removedElement, newSourceList] = removeFromList(sourceList, result.source.index)  
-		listCopy[result.source.droppableId] = newSourceList  
-	
-		const destinationList = listCopy[result.destination.droppableId]  
-		removedElement.listId = result.destination.droppableId; //* CHANGED THIS SO THAT THE ELEMENTS LIST ID IS DYNAMICALLY UPDATED. 
-		listCopy[result.destination.droppableId] = addToList(destinationList, result.destination.index, removedElement)
+		const listCopy = {...board} // Copies the state of the board
+		
+		// Creates a copy of the list the Draggable was taken from, removes the Draggable from that list, and overrides that list in the overall listCopy.
+		const sourceList: cardData[] = listCopy[result.source.droppableId]; // result.source.droppableId is the key of the key value pair of the list in the elements state object that the draggable was taken from
+		const [removedElement, newSourceList] = removeFromList(sourceList, result.source.index);
+
+		if (Array.isArray(newSourceList)) listCopy[result.source.droppableId] = newSourceList; 
+		const destinationList = listCopy[result.destination.droppableId];
+		if (!Array.isArray(removedElement)) {
+			removedElement.listId = result.destination.droppableId; //* CHANGED THIS SO THAT THE ELEMENTS LIST ID IS DYNAMICALLY UPDATED. 
+			listCopy[result.destination.droppableId] = addToList(destinationList, result.destination.index, removedElement);
+		}
 		setBoard(listCopy)
 	}
-  return (
-    <>
+
+  	return (
+    	<>
 			<div className='board-columns'> {/* If you want the react-dnd-board to be scrollable, the container outside the Droppables need to be scrollable. */}
 				<div className="board-area">
 					<KanbanBoardHeading/>
 
-
-
 					<DragDropContext onDragEnd={onDragEnd}>
-{/* https://github.com/atlassian/react-beautiful-dnd/issues/1734
-	potential solution
-*/}
-
 						<div className="board-columns-wrapper">
-							{Object.keys(board).map((list, index) => (
-								<div className="board-column">
+							{Object.keys(board).map((list: string, index) => (
+								<div className="board-column" key={list}>
 									<Droppable droppableId={list}> 
-										{(provided: any, snapshot: any) => (
+										{(provided: DroppableProvided) => (
 											<div 
 												className='board-droppable-area'
 												ref={provided.innerRef}
 												{...provided.droppableProps} 
 											>
 
-
-								
-												{board[list].map((card: any, index: any) => (
+												{/* ////////////////////////////////////////////////// */}
+												{board[list].map((card: cardData, index: number) => (
 													<Draggable key={card.id} draggableId={card.id} index={index}>
-														{(provided: any) => (
+														{(provided: DraggableProvided) => (
 															<div
 																ref={provided.innerRef}
 																{...provided.draggableProps}
@@ -255,9 +127,7 @@ export default function KanbanBoard() {
 														)}
 													</Draggable>
 												))}
-
-
-
+												{/* ////////////////////////////////////////////////// */}
 
 												{provided.placeholder}
 											</div>
@@ -267,14 +137,10 @@ export default function KanbanBoard() {
 							))}
 						</div>
 					</DragDropContext>
-
-
-
-
 				</div>
 			</div>
-    </>
-  )
+    	</>
+  	)
 }
 
 
@@ -285,7 +151,9 @@ export default function KanbanBoard() {
 
 
 
-
+{/* https://github.com/atlassian/react-beautiful-dnd/issues/1734
+	potential solution
+*/}
 
 
 
@@ -361,3 +229,136 @@ export default function KanbanBoard() {
 							))}
 						</div>
 					</DragDropContext> */}
+
+
+
+
+
+
+
+
+
+
+
+					// {
+					// 	id: '10',
+					// 	listId: 'backlog',
+					// 	priority: 'High',
+					// 	project: 'Velocity',
+					// 	title: 'Test2',
+					// 	tags: ['UI', "Backend"],
+					// 	deadline: '15 Jun 2022'
+					// },{
+					// 	id: '3',
+					// 	listId: 'backlog',
+					// 	priority: 'High',
+					// 	project: 'Velocity',
+					// 	title: 'Test3',
+					// 	tags: ['UI', "Backend"],
+					// 	deadline: '15 Jun 2022'
+					// },{
+					// 	id: '4',
+					// 	listId: 'backlog',
+					// 	priority: 'High',
+					// 	project: 'Velocity',
+					// 	title: 'Test4',
+					// 	tags: ['UI', "Backend"],
+					// 	deadline: '15 Jun 2022'
+					// },{
+					// 	id: '5',
+					// 	listId: 'backlog',
+					// 	priority: 'High',
+					// 	project: 'Velocity',
+					// 	title: 'Test5',
+					// 	tags: ['UI', "Backend"],
+					// 	deadline: '15 Jun 2022'
+					// },{
+					// 	id: '6',
+					// 	listId: 'backlog',
+					// 	priority: 'High',
+					// 	project: 'Velocity',
+					// 	title: 'Test6',
+					// 	tags: ['UI', "Backend"],
+					// 	deadline: '15 Jun 2022'
+					// },{
+					// 	id: '7',
+					// 	listId: 'backlog',
+					// 	priority: 'High',
+					// 	project: 'Velocity',
+					// 	title: 'Test7',
+					// 	tags: ['UI', "Backend"],
+					// 	deadline: '15 Jun 2022'
+					// },{
+					// 	id: '8',
+					// 	listId: 'backlog',
+					// 	priority: 'High',
+					// 	project: 'Velocity',
+					// 	title: 'Test8',
+					// 	tags: ['UI', "Backend"],
+					// 	deadline: '15 Jun 2022'
+					// },{
+					// 	id: '9',
+					// 	listId: 'backlog',
+					// 	priority: 'High',
+					// 	project: 'Velocity',
+					// 	title: 'Test9',
+					// 	tags: ['UI', "Backend"],
+					// 	deadline: '15 Jun 2022'
+					// },{
+					// 	id: '11',
+					// 	listId: 'backlog',
+					// 	priority: 'High',
+					// 	project: 'Velocity',
+					// 	title: 'Test11',
+					// 	tags: ['UI', "Backend"],
+					// 	deadline: '15 Jun 2022'
+					// },{
+					// 	id: '12',
+					// 	listId: 'backlog',
+					// 	priority: 'High',
+					// 	project: 'Velocity',
+					// 	title: 'Test12',
+					// 	tags: ['UI', "Backend"],
+					// 	deadline: '15 Jun 2022'
+					// }
+					// ,{
+					// 	id: '13',
+					// 	listId: 'backlog',
+					// 	priority: 'High',
+					// 	project: 'Velocity',
+					// 	title: 'Test13',
+					// 	tags: ['UI', "Backend"],
+					// 	deadline: '15 Jun 2022'
+					// },{
+					// 	id: '14',
+					// 	listId: 'backlog',
+					// 	priority: 'High',
+					// 	project: 'Velocity',
+					// 	title: 'Test14',
+					// 	tags: ['UI', "Backend"],
+					// 	deadline: '15 Jun 2022'
+					// },{
+					// 	id: '15',
+					// 	listId: 'backlog',
+					// 	priority: 'High',
+					// 	project: 'Velocity',
+					// 	title: 'Test15',
+					// 	tags: ['UI', "Backend"],
+					// 	deadline: '15 Jun 2022'
+					// },{
+					// 	id: '16',
+					// 	listId: 'backlog',
+					// 	priority: 'High',
+					// 	project: 'Velocity',
+					// 	title: 'Test16',
+					// 	tags: ['UI', "Backend"],
+					// 	deadline: '15 Jun 2022'
+					// },{
+					// 	id: '17',
+					// 	listId: 'backlog',
+					// 	priority: 'High',
+					// 	project: 'Velocity',
+					// 	title: 'Test17',
+					// 	tags: ['UI', "Backend"],
+					// 	deadline: '15 Jun 2022'
+					// }
